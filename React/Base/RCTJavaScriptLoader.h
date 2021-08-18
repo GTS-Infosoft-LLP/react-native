6a34b085935fd0b4d987a4b4db680b2fcd5cc728
Zyauddin <71919615+Zyauddin@users.noreply.github.com>
2021-08-18 16:27:26 +0530

    script loader react missing issue fixed

--- React/Base/RCTJavaScriptLoader.h
+++ React/Base/RCTJavaScriptLoader.h
@@ -7,7 +7,11 @@
 
 #import <UIKit/UIKit.h>
 
-#import <React/RCTDefines.h>
+#if __has_include("RCTDefines.h")
+  #import "RCTDefines.h"
+#else
+ #import <React/RCTDefines.h>
+#endif
 
 extern NSString *const RCTJavaScriptLoaderErrorDomain;
 
